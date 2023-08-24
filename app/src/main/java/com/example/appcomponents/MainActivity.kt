95d7d267fa2bb6083ddf71daf5075407c21453d3
thainasilvaz <thaina.silva254@etec.sp.gov.br>
2023-08-24 09:09:13 -0300

    Form using Items 'material3'

---
+++ app/src/main/java/com/example/appcomponents/MainActivity.kt
@@ -0,0 +1,519 @@
+package com.example.appcomponents
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.appcomponents.ui.theme.AppComponentsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppComponentsTheme {
+                App()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun App(){
+    AppComponentsTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                /*MenuTopBar()
+                Greeting(name = "Android")
+                */
+
+                FormLayoutFilled()
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 150, heightDp = 200)
+@Composable
+fun AppPreview(){
+    App()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuTopBar(){
+    Scaffold (
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color.Blue
+                ),
+                title = {
+                    Column {
+                        Text(text = "JetPack", color = Color.White)
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.ArrowBack, tint = Color.White, contentDescription = "" )
+                    }
+                },
+                actions = {
+                    Icon(imageVector = Icons.Filled.MoreVert, tint = Color.White, contentDescription = "")
+                }
+            )
+        }
+    ){
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)){
+
+        }
+    }
+}
+@Preview(showBackground = true, widthDp = 200, heightDp = 100)
+@Composable
+fun MenuTopBarPreview(){
+    MenuTopBar()
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppComponentsTheme {
+        Greeting("Android")
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun SimpleCenterAlignedTopAppBar() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        "Centered TopAppBar",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.AccountCircle,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            )
+        },
+        content = { innerPadding ->
+            LazyColumn(
+                contentPadding = innerPadding,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, widthDp = 400, heightDp = 1150)
+@Composable
+fun FormLayoutFilled() {
+    val focusManager = LocalFocusManager.current
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        contentPadding = PaddingValues(vertical = 24.dp)
+    ) {
+        item {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Surface(
+                    onClick = { /* TODO */ },
+                    color = MaterialTheme.colorScheme.surfaceVariant,
+                    shape = CircleShape
+                ) {
+                    Box(
+                        modifier = Modifier.size(96.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.AccountCircle,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    }
+                }
+                Text("Add Foto")
+            }
+        }
+        item {
+            var imageUrl = remember { mutableStateOf<String?>(null) }
+            Box(Modifier.fillMaxWidth()) {
+                AsyncImage(
+                    model = imageUrl,
+                    contentDescription = null
+                )
+            }
+        }
+        item {
+            var text by remember { mutableStateOf("") }
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                label = { Text("Nome Completo") },
+                value = text,
+                onValueChange = { text = it },
+                singleLine = true,
+                trailingIcon = {
+                    AnimatedVisibility(
+                        visible = text.isNotBlank(),
+                        enter = fadeIn(),
+                        exit = fadeOut()
+                    ) {
+                        IconButton(onClick = { text = "" }) {
+                            Icon(Icons.Outlined.Clear, "Clear")
+                        }
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next,
+                    capitalization = KeyboardCapitalization.Words
+                ),
+                keyboardActions = KeyboardActions {
+                    focusManager.moveFocus(FocusDirection.Next)
+                }
+            )
+
+        }
+        item { Spacer(Modifier.height(4.dp)) }
+        item {
+            var text by remember { mutableStateOf("") }
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                label = { Text("Número de Telefone") },
+                value = text,
+                onValueChange = { text = it },
+                trailingIcon = {
+                    AnimatedVisibility(
+                        visible = text.isNotBlank(),
+                        enter = fadeIn(),
+                        exit = fadeOut()
+                    ) {
+                        IconButton(onClick = { text = "" }) {
+                            Icon(Icons.Outlined.Clear, "Clear")
+                        }
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Phone,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions {
+                    focusManager.moveFocus(FocusDirection.Next)
+                },
+                singleLine = true,
+            )
+        }
+        item { Spacer(Modifier.height(4.dp)) }
+        item {
+            var text by remember { mutableStateOf("") }
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                label = { Text("Email") },
+                value = text,
+                onValueChange = { text = it },
+                trailingIcon = {
+                    AnimatedVisibility(
+                        visible = text.isNotBlank(),
+                        enter = fadeIn(),
+                        exit = fadeOut()
+                    ) {
+                        IconButton(onClick = { text = "" }) {
+                            Icon(Icons.Outlined.Clear, "Clear")
+                        }
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions {
+                    focusManager.clearFocus()
+                },
+                singleLine = true,
+            )
+        }
+
+        item {
+            var text by remember { mutableStateOf("") }
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                label = { Text("Endereço") },
+                value = text,
+                onValueChange = { text = it },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions {
+                    focusManager.moveFocus(FocusDirection.Next)
+                },
+                singleLine = true,
+                trailingIcon = {
+                    AnimatedVisibility(
+                        visible = text.isNotBlank(),
+                        enter = fadeIn(),
+                        exit = fadeOut()
+                    ) {
+                        IconButton(onClick = { text = "" }) {
+                            Icon(Icons.Outlined.Clear, "Clear")
+                        }
+                    }
+                },
+            )
+        }
+        item {
+            var text by remember { mutableStateOf("") }
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                label = { Text("Cidade") },
+                value = text,
+                onValueChange = { text = it },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions {
+                    focusManager.moveFocus(FocusDirection.Next)
+                },
+                singleLine = true,
+                trailingIcon = {
+                    AnimatedVisibility(
+                        visible = text.isNotBlank(),
+                        enter = fadeIn(),
+                        exit = fadeOut()
+                    ) {
+                        IconButton(onClick = { text = "" }) {
+                            Icon(Icons.Outlined.Clear, "Clear")
+                        }
+                    }
+                },
+            )
+        }
+        item { Spacer(Modifier.height(4.dp)) }
+        item {
+            var text by remember { mutableStateOf("") }
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                value = text,
+                onValueChange = {
+                    text = it
+                },
+                label = { Text("País") },
+                trailingIcon = {
+                    AnimatedVisibility(
+                        visible = text.isNotBlank(),
+                        enter = fadeIn(),
+                        exit = fadeOut()
+                    ) {
+                        IconButton(onClick = { text = "" }) {
+                            Icon(Icons.Outlined.Clear, "Clear")
+                        }
+                    }
+                },
+            )
+        }
+        item {
+            Text(
+                text = "Intereses:",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 24.dp,
+                    bottom = 8.dp
+                )
+            )
+        }
+        item {
+            var selected by remember { mutableStateOf<Int?>(null) }
+            val options = listOf("Dinheiro", "Amigos", "Familia", "Other")
+            Column {
+                options.forEachIndexed { i, option ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(24.dp),
+                        modifier = Modifier
+                            .clickable { selected = i }
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        RadioButton(
+                            selected = selected == i,
+                            onClick = null
+                        )
+                        Text(text = option)
+                    }
+
+                }
+            }
+        }
+        item {
+            Text(
+                text = "Opções de Notificações",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 24.dp,
+                    bottom = 8.dp
+                )
+            )
+        }
+        item {
+            var selected by remember { mutableStateOf(false) }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(24.dp),
+                modifier = Modifier
+                    .clickable { selected = selected.not() }
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Checkbox(
+                    checked = selected,
+                    onCheckedChange = null
+                )
+                Text(text = "Não pertube.")
+            }
+        }
+        item {
+            var selected by remember { mutableStateOf(false) }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(24.dp),
+                modifier = Modifier
+                    .clickable { selected = selected.not() }
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Checkbox(
+                    checked = selected,
+                    onCheckedChange = null
+                )
+                Text(text = "Permitir Emails e SMS.")
+            }
+            Column (modifier = Modifier, verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+                Button(onClick = { /* Do something! */ } ) { Text("Enviar") }
+            }
+
+        }
+    }
+
+}
+
+
+
+@Composable
+fun AsyncImage(model: MutableState<String?>, contentDescription: Nothing?) {
+
+}
+
+
